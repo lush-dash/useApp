f7cba83553e73a0eafa67d73eabc4b5792761a7c
Evgenii K <e.kaminskii1990@gmail.com>
2022-11-10 11:53:23 +0300

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-filename-extension': 0,
+    'no-use-before-define': 0,
+    'react/style-prop-object': 0,
+
+  },
+};
